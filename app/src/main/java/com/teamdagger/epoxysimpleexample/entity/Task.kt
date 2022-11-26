3a2f4620f273419d5ca6bc1807a190c778b25a56
zackmolt <fkrihkl@gmail.com>
2022-11-26 22:15:34 +0700

    refactor: add taskColor field

--- app/src/main/java/com/teamdagger/epoxysimpleexample/entity/Task.kt
+++ app/src/main/java/com/teamdagger/epoxysimpleexample/entity/Task.kt
@@ -1,7 +1,10 @@
 package com.teamdagger.epoxysimpleexample.entity
 
+import androidx.annotation.ColorInt
+
 data class Task (
     val title: String,
     val description: String,
-    val people: List<Person>
+    val people: List<Person>,
+    @ColorInt val taskColor: Int
 )
