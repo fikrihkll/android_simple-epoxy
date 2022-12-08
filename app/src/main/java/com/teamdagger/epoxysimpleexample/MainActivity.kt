dbed01a431f26266c1cfc737b393277ed315ddac
zackmolt <fkrihkl@gmail.com>
2022-12-08 23:35:58 +0700

    feat: configure epoxy recyclerview with controller

--- app/src/main/java/com/teamdagger/epoxysimpleexample/MainActivity.kt
+++ app/src/main/java/com/teamdagger/epoxysimpleexample/MainActivity.kt
@@ -3,6 +3,8 @@ package com.teamdagger.epoxysimpleexample
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.teamdagger.epoxysimpleexample.databinding.ActivityMainBinding
+import com.teamdagger.epoxysimpleexample.epoxy_controller.HomeController
+import com.teamdagger.epoxysimpleexample.util.DummyData
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,5 +15,12 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        val controller = HomeController(this)
+        binding.taskEpoxyRecyclerView.setController(controller)
+
+        controller.carouselTaskData = DummyData.carouselItems.toMutableList()
+        controller.taskData = DummyData.provideTaskItems(this).toMutableList()
     }
+
+
 }
